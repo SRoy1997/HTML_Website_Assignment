dc27bbe644ae242314189a77c7ec7c85fa42a71e
SRoy1997 <sarah@devpipeline.com>
2022-06-03 13:41:38 -0600

    First SCSS Project

---
+++ scripts/index.js
@@ -0,0 +1,11 @@
+const slider = document.getElementById("slider");
+
+const img = document.querySelector(".foreground-img");
+
+slider.addEventListener("input", (e) => {
+  const sliderValue = e.target.value;
+
+  img.style.value = `${sliderValue}%`;
+
+  console.log(sliderValue);
+});
